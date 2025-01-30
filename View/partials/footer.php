9ad8741c8d3f5e97ddf72c81be985171bb6dc5a4
AmaraABC <amara-noah.gakou@efrei.net>
2025-01-30 11:33:18 +0100

    Nouveau fichier contact.php et amélioration du footer et header

---
+++ View/partials/footer.php
@@ -0,0 +1,9 @@
+<footer class="owasp-footer">
+    <div class="owasp-copyright">
+        <p> &copy; SmartBike - <?php echo date('Y') ?></p>
+    </div>
+</footer>
+
+</body>
+
+</html>
---
+++ View/partials/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr-FR" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Responsive Design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site OWASP</title>
+    <meta name="OWASP" content="">
+    <!-- Ajout de la feuille de style -->
+    <link rel="stylesheet" href="/View/style/style.css">
+</head>
+
+<body>
+    <!-- En-tête de page contenant le logo et les différents liens du site -->
+    <header class="owasp-header">
+        <!-- Logo de l'enseigne -->
+        <a href="/index.php" class="owasp-logo">
+            <strong>OWASP</strong>
+        </a>
+        <!-- Liens pour naviguer entre chaque page du site -->
+        <nav class="owasp-links">
+            <!-- Liens -->
+            <ul class="owasp-pages">
+                <li>
+                    <a href="/index.php?page=homepage">Accueil</a>
+                </li>
+                <li>
+                    <a href="/index.php?page=articles">Produits</a>
+                </li>
+                <li>
+                    <a href="/index.php?page=contacts">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
