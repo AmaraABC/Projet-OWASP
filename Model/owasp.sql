e656fd4b76760d2b8c20d02f283a607aff00a134
cryo-ghost <159237374+cryo-ghost@users.noreply.github.com>
2025-01-30 11:16:18 +0100

    Update owasp.sql

--- Model/owasp.sql
+++ Model/owasp.sql
@@ -2,16 +2,16 @@ CREATE DATABASE Owasp;
 USE Owasp;
 
 
-DROP TABLE IF EXISTS `product` ;
+DROP TABLE IF EXISTS 'product' ;
 
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL COMMENT 'Le nom du produit',
-  `description` TEXT NULL COMMENT 'La description du produit',
-  `picture` VARCHAR(128) NULL COMMENT 'L\'URL de l\'image du produit',
-  `price` DECIMAL(10,2) NOT NULL DEFAULT 0 COMMENT 'Le prix du produit',
-  `status` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Le statut du produit (0=non renseignée, 1=dispo, 2=pas dispo)',
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'La date de création du produit',
+CREATE TABLE IF NOT EXISTS 'product' (
+  'id' INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  'name' VARCHAR(45) NOT NULL,
+  'description' TEXT NULL,
+  'picture' VARCHAR(128) NULL,
+  'price' DECIMAL(10,2) NOT NULL DEFAULT 0,
+  'status' TINYINT(1) NOT NULL DEFAULT 0,
+  'created_at' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 )
 
 
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS `product` (
 --les espace entre les valeur sont juste là pour faire beau
 
 --START TRANSACTION;
---INSERT INTO `product` (`id`, `name`, `description`, `picture`, `price`, `status`, `created_at`) VALUES (
---      `id`, `nom`,         `description`,           `image`,                          `prix`, `status`, `date`
+--INSERT INTO 'product' ('id', 'name', 'description', 'picture', 'price', 'status', 'created_at') VALUES (
+--      'id', 'nom',         'description',           'image',                          'prix', 'status', 'date'
 --      '1',  'nomproduit', 'description du produit', '../image/produit/nomproduit.jpg', '30',   '1',     '2018-10-17'
 --);
 --COMMIT;
