083cc4b772cbe9479bcb609ac6143f77a2fbc24b
AmaraABC <amara-noah.gakou@efrei.net>
2025-01-30 11:52:21 +0100

    Ajout de quelques lignes de code dans le fichier router.php

---
+++ Controller/router.php
@@ -0,0 +1,8 @@
+<?php
+
+// Définition d'une variable pour la redirection vers une page du site
+$page = (isset($_GET['page']) ? $_GET['page'] : 'homepage');
+
+switch ($page) {
+    case 'homepage':
+}
