beea68ae07480eee98984bcaa28b71b0df1c4ebb
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-04 08:55:22 +0100

    Ajout de nouvelles lignes de code pour le fichier productController.php

--- Controller/productController.php
+++ Controller/productController.php
@@ -1,1 +1,27 @@
 <?php
+include_once('./Model/productModel.php');
+
+// Classe pour afficher les informations de chaque View
+class productController
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new productModel;
+    }
+
+    // Transmission des informations à la page qui affiche les produits du microsite
+    public function articles()
+    {
+        $articles = $this->model->getAllProducts();
+        include('./View/articles.php');
+    }
+
+    // Transmission des informations à la page 'details' des produits
+    public function details()
+    {
+        $details = $this->model->getProductById($_GET['id']);
+        include('./View/details.php');
+    }
+}
