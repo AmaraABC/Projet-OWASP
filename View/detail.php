88bdf3c691107e1b5dd48460d57dd76e07e0e018
AmaraABC <amara-noah.gakou@efrei.net>
2025-03-06 00:24:42 +0100

    Modification de la page 'produits' et 'detail'

---
+++ View/detail.php
@@ -0,0 +1,27 @@
+<h1 class="page-title">Détail</h1>
+
+<!-- Afficher tous les détails disponibles d'un produit -->
+<div class="owasp-product-detail">
+    <?php foreach ($details as $detail) { ?>
+        <section class="owasp-product">
+            <article class="owasp-product-img">
+                <?php
+                echo '<a href="/index.php?page=details&id=' . $detail['id'] . '">
+                    <img src="' . $detail['picture'] . '" alt="' . $detail['alt'] . '">
+                    </a>';
+                ?>
+            </article>
+            <article class="owasp-product-text">
+                <h2>
+                    <?php echo $detail['name']; ?>
+                </h2>
+                <p>
+                    Date d'ajout du produit : <?php echo $detail['created_at']; ?>
+                </p>
+                <p>
+                    <?php echo $detail['price']; ?> €
+                </p>
+            </article>
+        </section>
+    <?php } ?>
+</div>
